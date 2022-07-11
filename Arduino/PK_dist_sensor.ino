c6b9f67635e476b5ea1d15374c9ba11f2e17c106
jawa8653 <109106488+jawa8653@users.noreply.github.com>
2022-07-11 16:13:30 -0600

    Add files via upload

---
+++ Arduino/PK_dist_sensor.ino
@@ -0,0 +1,63 @@
+/*
+  SparkFun Inventor’s Kit
+  Circuit 3B-Distance Sensor
+
+  Control the color of an RGB LED using an ultrasonic distance sensor.
+
+  This sketch was written by SparkFun Electronics, with lots of help from the Arduino community.
+  This code is completely free for any use.
+
+  View circuit diagram and instructions at: https://learn.sparkfun.com/tutorials/sparkfun-inventors-kit-experiment-guide---v40
+  Download drawings and code at: https://github.com/sparkfun/SIK-Guide-Code
+
+
+  MODIFIICATIONS FOR PENALTY KICK ROBOT
+  LAST MODIFIED: 7/11/22
+  MODIFIED BY: Jamie Warren
+  MODIFICATIONS:
+    removed all code for LED control
+*/
+
+const int trigPin = 11;           //connects to the trigger pin on the distance sensor
+const int echoPin = 12;           //connects to the echo pin on the distance sensor
+
+float distance = 0;               //stores the distance measured by the distance sensor
+
+void setup()
+{
+  Serial.begin (9600);        //set up a serial connection with the computer
+
+  pinMode(trigPin, OUTPUT);   //the trigger pin will output pulses of electricity
+  pinMode(echoPin, INPUT);    //the echo pin will measure the duration of pulses coming back from the distance sensor
+ 
+}
+
+void loop() {
+  distance = getDistance();   //variable to store the distance measured by the sensor
+
+  Serial.print(distance);     //print the distance that was measured
+  Serial.println(" in");      //print units after the distance
+
+  delay(50);      //delay 50ms between each reading
+}
+
+//------------------FUNCTIONS-------------------------------
+
+//RETURNS THE DISTANCE MEASURED BY THE HC-SR04 DISTANCE SENSOR
+float getDistance()
+{
+  float echoTime;                   //variable to store the time it takes for a ping to bounce off an object
+  float calculatedDistance;         //variable to store the distance calculated from the echo time
+
+  //send out an ultrasonic pulse that's 10ms long
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  echoTime = pulseIn(echoPin, HIGH);      //use the pulsein command to see how long it takes for the
+                                          //pulse to bounce back to the sensor
+
+  calculatedDistance = echoTime / 148.0;  //calculate the distance of the object that reflected the pulse (half the bounce time multiplied by the speed of sound)
+
+  return calculatedDistance;              //send back the distance that was calculated
+}
