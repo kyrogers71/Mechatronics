c98088d87a19c6f1e7ece79f6c0579fd5872637f
Kyle Rogers <85505338+kyrogers71@users.noreply.github.com>
2022-07-18 16:00:03 -0600

    Update LED_Offense_Defense.ino

--- Arduino/LED_Offense_Defense.ino
+++ Arduino/LED_Offense_Defense.ino
@@ -1,10 +1,14 @@
 //this sketch will output states and illuminate LEDs based on offense or defense mode
+//Version 2, final
 
-const int redLED = 8;
+const int redLED = 10;
 const int greenLED = 9;
 const int button = 6;
+const int outputState = 2;
 //define pinouts
+
 int buttonState = 0;
+int robotState = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -18,22 +22,30 @@ void setup() {
 }
 
 void loop() {
- // read the state of the pushbutton value:
-  buttonState = digitalRead(button);
-  Serial.print(buttonState);
-  delay(100);
+ // read the state of the pushbutton value:;
+  //Serial.print(buttonState);
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    Serial.print("HIGH");
+  if (digitalRead(button) == HIGH) {
     ++buttonState;
+    delay(100);
   }
-  if (buttonState == 1){
+  if ((buttonState & 1) == 0){
     // turn red LED on (defense):
     digitalWrite(redLED, HIGH);
     digitalWrite(greenLED, LOW);
+    robotState = 0;
+    delay(100);
   } else {
     // turn green LED on (offense):
     digitalWrite(greenLED, HIGH);
     digitalWrite(redLED, LOW);
+    robotState = 1;
+    delay(100);
   }
+  Serial.print(robotState);
+  if (robotState ==0){
+    digitalWrite(outputState, LOW);}
+  else {
+    digitalWrite(outputState, HIGH);}
+
 }
